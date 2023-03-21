dc9f217d2487c42b167a13338c71ef91b966fc93
Opi22 <khideromer20031610@gmail.com>
2023-03-21 20:09:26 +0200

    play round done

--- script.js
+++ script.js
@@ -5,4 +5,23 @@ function getComputerChoice () {
 }
 function playRound (player, computer){
     player = player .toLowerCase();
+    if (player==="rock" && computer ==="rock"){
+        return "it is a draw";
+    } else if (player==="rock" && computer ==="paper"){
+          return "player lost";
+    } else if (player==="rock" && computer==="scissors"){
+        return "player won";
+    } else if (player ==="paper" && computer ==="rock"){
+        return "player won";
+    } else if (player ==="paper" && computer ==="paper"){
+        return "it is a draw";
+    } else if (player=== "paper" && computer === "scissors"){
+        return "player lost";
+    }else if (player ==="scissors" && computer ==="rock"){
+        return "player lost"; 
+    } else if (player ==="scissors" && computer ==="paper"){
+        return "player won";
+    } else if (player ==="scissors" && computer ==="scissors"){
+        return "it is a draw";
+    }
 }

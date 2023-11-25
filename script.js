ddcf8c27dd4c30b419dcbb0cbd015fc67cad68d9
Opi22 <khideromer20031610@gmail.com>
2023-11-25 10:34:40 +0200

    start a new css file

--- script.js
+++ script.js
@@ -25,29 +25,4 @@ function playRound (player, computer){
         return "it is a draw";
     }
 }
-function game(){
-    let scoreWin=0;
-    let scoreLose = 0;
-    let scoreDraw=0;
-    for (let i=0;  i<5;  i++){
-       let  playerChoice = prompt("Rock, Paper, Scissors shoot!!!!!");
-       let computerChoice= getComputerChoice();
-       let roundResult = playRound(playerChoice,  computerChoice);
-       console.log(roundResult);
-       if (roundResult=== "player won"){
-        scoreWin= scoreWin+1;
-       } else if (roundResult==="player lost"){
-        scoreLose= scoreLose+1;
-       } else if (roundResult==="it is a draw"){
-        scoreDraw= scoreDraw+1;
-       }
-    }
-    if (scoreWin>2){
-        console.log("you won the game");
-    } else if (scoreLose>2){
-        console.log("you lost the game!!!");
-    } else {
-        console.log("it is a draw");
-    }
-}
-game();
+
